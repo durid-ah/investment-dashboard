0167a6e8d56cc22801ca08828d7444286f477a65
durid-ah <ahmad.durid96@gmail.com>
2024-08-07 19:49:12 -0400

    move investment category table to diesel migration

---
+++ src-tauri/migrations/2024-08-07-234441_create_investment_category/up.sql
@@ -0,0 +1,3 @@
+CREATE TABLE IF NOT EXISTS investment_category (
+  category TEXT PRIMARY KEY
+);
--- src-tauri/src/db/init_db.rs
+++ src-tauri/src/db/init_db.rs
@@ -3,39 +3,7 @@ use anyhow::Result;
 use super::utils::create_connection;
 
 pub fn initialize() -> Result<()> {
-    create_ticker_table()?;
-    create_investment_category_table()?;
     create_investment_table()?;
-
-    Ok(())
-}
-
-
-
-fn create_ticker_table() -> Result<()> {
-    let conn = create_connection();
-    conn.execute(
-        "
-        CREATE TABLE IF NOT EXISTS ticker (
-            ticker TEXT PRIMARY KEY
-        );
-    ",
-        (),
-    )?;
-
-    Ok(())
-}
-
-fn create_investment_category_table() -> Result<()> {
-    let conn = create_connection();
-    conn.execute(
-        "
-        CREATE TABLE IF NOT EXISTS investment_category (
-            category TEXT PRIMARY KEY
-        );
-        ",
-        (),
-    )?;
 
     Ok(())
 }
