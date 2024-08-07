5e7cfff889e15bf03a2ee4edf2aedd6bf93d7866
durid-ah <ahmad.durid96@gmail.com>
2024-08-07 19:51:28 -0400

    move investment table to diesel migration

---
+++ src-tauri/migrations/2024-08-07-234929_create_investment/up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE investment (
+  id INTEGER PRIMARY KEY,
+  account_id INTEGER NOT NULL,
+  ticker TEXT NOT NULL,
+  shares REAL NOT NULL,
+  value REAL NOT NULL,
+  category TEXT NULL,
+  FOREIGN KEY (account_id) REFERENCES account(id),
+  FOREIGN KEY (ticker) REFERENCES ticker(ticker),
+  FOREIGN KEY (category) REFERENCES investment_category(category)
+);
