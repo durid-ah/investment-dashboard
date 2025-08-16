20e51e5dbce839c10ba49f36e692245247287cde
Durid Ahmad <durid.ahmad.dev@gmail.com>
2025-08-16 19:28:16 -0400

    getting started on the editable ticker

--- src/app/components/editable-ticker.tsx
+++ src/app/components/editable-ticker.tsx
@@ -1,16 +1,16 @@
 'use client'
 
-import { HTMLInputTypeAttribute, useState } from "react"
+import { useState } from "react"
+import TickerDropdown from "./ticker-dropdown";
 
 type Value = string | number | readonly string[] | undefined;
 
-type EditableValueProps = {
+type EditableTickerProps = {
     content: Value,
-    type: HTMLInputTypeAttribute,
     onChange?: (value: Value) => void
 }
 
-export function EditableValue({content, type, onChange} : EditableValueProps) {
+export function EditableTicker({content, onChange} : EditableTickerProps) {
     const [isEditMode, setIsEditMode] = useState(false)
     const [field, setField] = useState(content)
 
@@ -31,12 +31,17 @@ export function EditableValue({content, type, onChange} : EditableValueProps) {
 
     return (
         <>
-            <input autoFocus type={type} 
-                ref={input => input?.focus()}
-                className="w-48"
-                hidden={!isEditMode} value={field}
-                onChange={(e) => onChangeHandler(e.target.value)} 
-                onBlur={() => onBlurHandler()}/>
+            {isEditMode && (
+                <TickerDropdown 
+                    onChange={(ticker) => onChangeHandler(ticker)}
+                />
+            // <input autoFocus type={type} 
+            //     ref={input => input?.focus()}
+            //     className="w-48"
+            //     hidden={!isEditMode} value={field}
+            //     onChange={(e) => onChangeHandler(e.target.value)} 
+            //     onBlur={() => onBlurHandler()}/>
+            )}
             <div hidden={isEditMode} className="w-48" 
                 onClick={() => divClickedHandler()}>
                 {field}
